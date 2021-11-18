306e765361a6043c749dd24168f0c81de5d5a20a
adam <b10709055@gapps.ntust.edu.tw>
2021-11-18 10:40:00 +0800

    Initial Commit

---
+++ Restaurant_Comment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Restaurant_Comment
+//
+//  Created by Adam on 2021/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
